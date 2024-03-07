527f97e9839b438ae0e18af3a6c054c3487afd57
ayslana <ayslanariene@gmail.com>
2024-03-07 08:57:32 -0300

    create fact solot component

---
+++ components/FactSlot.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { useCatFactsStore } from "@/stores/cat_facts_store";
+
+const catFactsStore = useCatFactsStore();
+const fadeInOut = ref(false);
+
+function toggleFadeInOut() {
+  fadeInOut.value = !fadeInOut.value;
+  setTimeout(() => {
+    fadeInOut.value = !fadeInOut.value;
+  }, 1000);
+  catFactsStore.chooseRandomFact();
+}
+</script>
+
+<template>
+  <div class="h-full flex flex-col justify-between px-12 mx-12">
+    <div
+      class="flex flex-col text-purple-darker text-3xl font-bold uppercase py-6"
+      title="Cat Fact"
+    >
+      <p>Cat Fact:</p>
+    </div>
+    <div
+      class="flex flex-col text-purple-medium text-2xl py-6 min-h-48 max-h-48 overflow-y-auto"
+    >
+      <p v-if="!catFactsStore.currentFact">
+        <span>Loading...</span>
+      </p>
+      <p :class="{ 'fade-in': fadeInOut }">
+        <span :title="catFactsStore.currentFact">
+          {{ catFactsStore.currentFact }}
+        </span>
+      </p>
+    </div>
+    <div class="pt-12 mt-12">
+      <DefaultButton
+        label="Get A Random Catfact"
+        hasPrependIcon
+        @click="toggleFadeInOut"
+        title="Get A Random Catfact Button"
+      >
+        <template #prepend-icon>
+          <Icon
+            class="mr-2"
+            name="material-symbols:refresh"
+            color="text-rose-lighter"
+          />
+        </template>
+      </DefaultButton>
+    </div>
+  </div>
+</template>
+
+<style>
+.fade-in {
+  animation: fadeIn 1s ease-in-out;
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
