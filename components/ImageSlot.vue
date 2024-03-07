bd63a61146b01cd00980624fd8607f1341dd1772
ayslana <ayslanariene@gmail.com>
2024-03-07 08:57:44 -0300

    create image slot component

---
+++ components/ImageSlot.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  image: String,
+});
+</script>
+
+<template>
+  <img
+    class="rounded-[20px] drop-shadow-2xl h-auto"
+    :src="props.image"
+    alt="Image"
+    :title="props.image"
+  />
+</template>
+
+<style></style>
